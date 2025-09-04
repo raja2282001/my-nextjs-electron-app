84bf71d2f0c7c1f1283dc386b3dbc49dceaa4a3b
parthpateltechein <parthpatel@techein.com>
2025-09-04 17:56:56 +0530

    change first

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const startUrl ="http://localhost:3000";
+
+  mainWindow.loadURL(startUrl);
+
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
